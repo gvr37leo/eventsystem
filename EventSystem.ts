aa66c8619d7bb916c6ceba6ba8cc1c1dbb62616f
Paul Boon <gvr37.p.l.boon@gmail.com>
2019-04-29 11:12:23 +0200

    p

--- EventSystem.ts
+++ EventSystem.ts
@@ -2,6 +2,7 @@ class Box<T>{
     onchange: EventSystem<T>
     onClear: EventSystemVoid
     value: T
+    oldValue:T
     isSet: boolean = false
 
     constructor(value: T) {
@@ -15,12 +16,12 @@ class Box<T>{
     }
 
     set(value: T, silent: boolean = false) {
-        var old = this.value
+        this.oldValue = this.value
         this.value = value
-        if (old != value || !this.isSet) {
+        if (this.oldValue != value || !this.isSet) {
             this.isSet = true;
             if (!silent) {
-                this.onchange.trigger(this.value,old)
+                this.onchange.trigger(this.value,this.oldValue)
             }
         }
     }
@@ -29,6 +30,10 @@ class Box<T>{
         this.isSet = false
         this.set(null)
         this.onClear.trigger()
+    }
+
+    boxtrigger(){
+        this.onchange.trigger(this.value,this.oldValue)
     }
 }
 
@@ -105,35 +110,48 @@ class PEvent<T>{
     constructor(public val:T){
 
     }
+
+    static create<T>(handled:boolean,val:T){
+        var e = new PEvent(val)
+        e.handled = handled
+        return e
+    }
 }
 
 class PBox<T>{
-    private box:Box<PEvent<T>>
-    onchange:EventSystem<PEvent<T>>
+    box:Box<T>
+    onchange:EventSystem<PEvent<T>> = new EventSystem()
+    private isProtected:boolean = false
 
     constructor(val:T){
-        this.box = new Box(new PEvent(val))
-        this.onchange = this.box.onchange
+        this.box = new Box(val)
+        this.box.onchange.listen((val,old) => {
+            this.onchange.trigger(PEvent.create(this.isProtected,val),null)
+        })
     }
 
     get():T{
-        return this.box.value.val
+        return this.box.value
     }
 
     set(v:T){
-        var e = new PEvent(v)
-        e.handled = false
-        this.box.set(e)
+        this.box.set(v)
     }
 
-    setS(e:PEvent<T>){
-        this.box.set(e)
+    setHP(handled:boolean,val:T){
+        this.setProtected(PEvent.create(handled,val))
     }
 
-    setH(e:PEvent<T>){
+    setProtected(e:PEvent<T>){
         if(!e.handled){
             e.handled = true
             this.setS(e)
         }
+    }
+
+    setS(e:PEvent<T>){
+        this.isProtected = e.handled
+        this.box.set(e.val)
+        this.isProtected = false
     }
 }
