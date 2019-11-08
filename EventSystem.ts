2fc0ea156217c1f1c751882e8cf4119038c1e5f0
Paul Boon <gvr37.p.l.boon@gmail.com>
2019-11-08 23:01:03 +0100

    p

--- EventSystem.ts
+++ EventSystem.ts
@@ -1,157 +1,75 @@
+class BoxEvent<T>{
+    constructor(public val:T, public old:T){
+
+    }
+}
+
 class Box<T>{
-    onchange: EventSystem<T>
-    onClear: EventSystemVoid
+    onchange: EventSystem<BoxEvent<T>>
     value: T
     oldValue:T
-    isSet: boolean = false
 
     constructor(value: T) {
         this.onchange = new EventSystem();
         this.value = value
-        this.onClear = new EventSystemVoid();
     }
 
     get(): T {
         return this.value
     }
 
-    set(value: T, silent: boolean = false) {
+    set(value: T) {
+        this.continueSet(value,new PEvent(new BoxEvent(value,this.value)))
+    }
+
+    continueSet(value: T, e: PEvent<BoxEvent<any>>){
         this.oldValue = this.value
         this.value = value
-        if (this.oldValue != value || !this.isSet) {
-            this.isSet = true;
-            if (!silent) {
-                this.onchange.trigger(this.value,this.oldValue)
-            }
+        e.val.old = this.oldValue
+        e.val.val = this.value
+        if (this.oldValue != value) {
+            this.continueTrigger(e)
         }
     }
 
-    clear() {
-        this.isSet = false
-        this.set(null)
-        this.onClear.trigger()
+    continueTrigger(e: PEvent<BoxEvent<T>>){
+        this.onchange.continueTrigger(e)
     }
 
-    boxtrigger(){
-        this.onchange.trigger(this.value,this.oldValue)
-    }
-}
-
-class EventSystem<T>{
-    callbacks: ((val: T, old:T) => void)[] = []
-
-    constructor() {
-
-    }
-
-    listen(callback: (val: T, old:T) => void) {
-        this.callbacks.push(callback)
-    }
-
-    deafen(callback: (val: T, old:T) => void) {
-        this.callbacks.splice(this.callbacks.findIndex(v => v === callback), 1)
-    }
-
-    trigger(value: T, old:T) {
-        for (var callback of this.callbacks) {
-            callback(value,old)
-        }
-    }
-}
-
-class EventSystemVoid{
-    callbacks: (() => void)[] = []
-
-    constructor() {
-
-    }
-
-    listen(callback: () => void) {
-        this.callbacks.push(callback)
-    }
-
-    deafen(callback: () => void) {
-        this.callbacks.splice(this.callbacks.findIndex(v => v === callback), 1)
-    }
-
-    trigger() {
-        for (var callback of this.callbacks) {
-            callback()
-        }
-    }
-}
-
-class ObjectBox<T>{
-    val:T
-    isSet: boolean = false
-    onChange:EventSystemVoid
-
-    constructor(val:T){
-        this.val = val
-    }
-
-    get<V>(selector:(obj:T) => Box<V>):V{
-        return selector(this.val).get()
-    }
-
-    set<V>(selector:(obj:T) => Box<V>, val:V){
-        var old = selector(this.val)
-        old.set(val)
-        if(old.get() != val || !this.isSet){
-            this.isSet = true
-            this.onChange.trigger()
-        }
+    trigger(){
+        this.continueTrigger(new PEvent(new BoxEvent(this.value,this.oldValue)))
     }
 }
 
 class PEvent<T>{
-    handled = false
-
+    cbset:Set<(val:PEvent<T>) => void> = new Set()
     constructor(public val:T){
 
     }
 
-    static create<T>(handled:boolean,val:T){
+    static create<T>(val:T){
         var e = new PEvent(val)
-        e.handled = handled
         return e
     }
 }
 
-class PBox<T>{
-    box:Box<T>
-    onchange:EventSystem<PEvent<T>> = new EventSystem()
-    private isProtected:boolean = false
+class EventSystem<T>{
+    cbs:((val:PEvent<T>) => void)[] = []
 
-    constructor(val:T){
-        this.box = new Box(val)
-        this.box.onchange.listen((val,old) => {
-            this.onchange.trigger(PEvent.create(this.isProtected,val),null)
-        })
+    listen(cb:(val:PEvent<T>) => void){
+        this.cbs.push(cb)
+        return cb
     }
 
-    get():T{
-        return this.box.value
+    trigger(val:T){
+        this.continueTrigger(new PEvent(val))
     }
 
-    set(v:T){
-        this.box.set(v)
-    }
-
-    setHP(handled:boolean,val:T){
-        this.setProtected(PEvent.create(handled,val))
-    }
-
-    setProtected(e:PEvent<T>){
-        if(!e.handled){
-            e.handled = true
-            this.setS(e)
+    continueTrigger(e:PEvent<T>){
+        for (var cb of this.cbs) {
+            if(e.cbset.has(cb))continue
+            e.cbset.add(cb)
+            cb(e)
         }
-    }
-
-    setS(e:PEvent<T>){
-        this.isProtected = e.handled
-        this.box.set(e.val)
-        this.isProtected = false
     }
 }
